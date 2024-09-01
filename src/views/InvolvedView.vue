a5e201f303a97567a0ff2af4cdcbb3d7d91c2d7d
QuentinQ <quentin.zt.qian@gmail.com>
2024-09-01 17:13:24 +1000

    update a basic page of other view

--- src/views/InvolvedView.vue
+++ src/views/InvolvedView.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <div class="container">
+    <div class="container">
+      <div>
+        <h1 class="text-center">Involved</h1>
+        <p class="text-center mb-3">Welcome to the Involved page!</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
--- src/views/NewsAndEventsView.vue
+++ src/views/NewsAndEventsView.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <div class="container">
+    <div class="container">
+      <div>
+        <h1 class="text-center">News and Events</h1>
+        <p class="text-center mb-3">Welcome to the News and Events page!</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
