be22f0f40e74a4297e467b438aef695e56438d76
QuentinQ <quentin.zt.qian@gmail.com>
2024-09-05 10:43:21 +1000

    fix community visavle by role

--- src/views/AccessDeniedView.vue
+++ src/views/AccessDeniedView.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="text-center">Access Denied</h1>
-      <!-- not log in -->
+      <!-- user not log in -->
       <div v-if="!isAuthenticated">
+        <h1 class="text-center">Access Denied</h1>
         <p class="text-center">
           You do not have permission to view this page. Please log in first.
         </p>
       </div>
-      <!-- logged in -->
+      <!-- user logged in -->
       <div v-else-if="currentRole == 'user'">
+        <h1 class="text-center">Access Denied</h1>
         <p class="text-center">You do not have permission to view this page.</p>
+      </div>
+      <!-- admin logged in -->
+      <div v-else-if="currentRole == 'admin'">
+        <h1 class="text-center">Community</h1>
+        <p class="text-center">Welcome to the community page!</p>
       </div>
     </div>
   </div>
--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,27 +1,13 @@
 <template>
   <div class="container">
-    <!-- Loged in -->
-    <div v-if="isAuthenticated" class="c-authenticated">
-      <div>
-        <h1 class="text-center">Community</h1>
-        <p class="text-center mb-3">Welcome to the community page!</p>
-      </div>
-    </div>
-    <!-- Not login -->
-    <div v-else class="c-not-authenticated">
-      <div>
-        <h1 class="text-center">Access Denied</h1>
-        <p class="text-center">
-          You do not have permission to view this page. Please log in first.
-        </p>
-      </div>
-    </div>
+    <access-denied-view />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useAuth } from '@/firebase/authenticate'
+import AccessDeniedView from './AccessDeniedView.vue'
 
 const { isAuthenticated } = useAuth()
 </script>
