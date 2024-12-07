cedb8ce653a954cc62bbd8be384271dd85c7a3a4
QuentinQ <quentin.zt.qian@gmail.com>
2024-12-08 00:08:06 +1100

    update config

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  
-  base: process.env.NODE_ENV === 'production' ? '/zqian-library/' : '/'
+
+  base: process.env.NODE_ENV === 'production' ? '/FIT5032-SoulClam/' : '/'
 })
